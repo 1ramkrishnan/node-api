56c096ff406b7319ed34e09d1497f2aa152188a7
rk <rktheaver@gmail.com>
2021-01-04 18:42:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+	const response = fetch("https://api.quotable.io/random");
+    const data = await response.json();
+     
+	res.json({ message: data});	
 });
 
 // on routes that end in /bears
